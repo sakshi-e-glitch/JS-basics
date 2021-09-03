b301c006292a17d8fefb45814270f61e1cfc7378
Sakshi <sakshi.pandey1510@gmail.com>
2021-09-03 20:18:54 +0530

    Logical Operators

--- app.js
+++ app.js
@@ -62,14 +62,42 @@
 // console.log(typeof a)
 
 
-//COMPARISION
-const a= 21
-const b= 7
-const c = '21'
+//COMPARISION OPERATORS-----------------------------------------------------------------------------------------------------------------------------------------
+// const a= 21
+// const b= 7
+// const c = '21'
 
-console.log('a>b', a>b)
-console.log('a<b', a<b)
-console.log('a==b', a==b)
-console.log('a==c', a==c)
+// console.log('a>b', a>b)
+// console.log('a<b', a<b)
+// console.log('a==b', a==b)
+// console.log('a==c', a==c)
+// console.log('a===c', a===c)
 
-console.log('a===c', a===c)
+//LOGICAL OPERATORS-------------------------------------------------------------------------------------------------------------------------------------------
+const a = 10
+const b = 50
+
+const c= 35
+
+console.log( c>a && c<b)
+
+console.log('a == b', a == b)
+console.log('a != b', a != b)
+console.log('')
+
+console.log('Truth table for AND')
+console.log('')
+
+console.log('false && false', false && false)
+console.log('true && true', true && true)
+console.log('true && false', true && false)
+console.log('false && true', false && true)
+console.log('')
+
+console.log('Truth table for OR')
+console.log('')
+
+console.log('false|| false', false || false)
+console.log('true|| true', true || true)
+console.log('true|| false', true || false)
+console.log('false|| true', false || true)
