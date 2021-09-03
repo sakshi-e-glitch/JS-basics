8527c7f8da686664086ba07afb52f55b1f22fdbc
Sakshi <sakshi.pandey1510@gmail.com>
2021-09-03 19:15:41 +0530

    Starting operators

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
-console.log("Hey this is my first piece of js code")
-console.log(123456789)
+// console.log("Hey this is my first piece of js code")
+// console.log(123456789)
 
-console.warn("This is a warning")
+// console.warn("This is a warning")
 
-console.error(("This is an error"))
+// console.error(("This is an error"))
+
+
+// DATATYPES
+// Primitive data types
+
+// var Name = "Nishtha"
+// console.log(Name)
+
+// let age=19
+// console.log(typeof age) 
+// let Name = 'a'
+// console.log(typeof Name)
+// let brand=null
+// console.log(brand)
+
+
+
+//Non primitive data types
+// let arr = [1,2,3,4,5]
+// arr.push("value")
+// arr.push(false)
+// console.log(arr)
+
+let Name= "Sakshi"
+let age = 19
+console.log(Name.toUpperCase)
+console.log(age. )
